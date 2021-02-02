8ff2f3c1a11cfbc8db173a52ca672efb4347ad27
Carlos Cueto <c.cueto@imperial.ac.uk>
2021-02-02 10:48:29 +0000

    Partial progress

---
+++ scripts/install_young.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+https://repo.anaconda.com/archive/Anaconda3-latest-Linux-x86_64.sh
+
+module load python/miniconda3
+git clone https://github.com/trustimaging/stride.git
+cd stride
