327fa63a77e96ae861ed74e07f229e551ada337e
Carlos Cueto <c.cueto@imperial.ac.uk>
2023-01-17 10:15:17 +0000

    Ignored devito import errors

--- stride/physics/common/stride_devito.py
+++ stride/physics/common/stride_devito.py
@@ -1,8 +1,8 @@
 
-from devito import *
+from devito import *  # noqa: F401
 
 try:
-    from devitopro import *
+    from devitopro import *  # noqa: F401
     pro_available = True
 except ImportError:
     pro_available = False
