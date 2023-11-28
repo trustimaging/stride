2679b603101032344bc6bc70b3936cc47489c7e5
Carlos Cueto <c.cueto@imperial.ac.uk>
2023-11-28 17:07:14 +0000

    Try to prevent OOB numa

--- mosaic/runtime/node.py
+++ mosaic/runtime/node.py
@@ -1,3 +1,4 @@
+import os
 
 import psutil
 
@@ -91,45 +92,52 @@ class Node(Runtime):
 
         if numa_available:
             available_cpus = numa.info.numa_hardware_info()['node_cpu_info']
+            allowed_cpus = numa.schedule.get_allowed_cpus_num()
+
         else:
             available_cpus = {worker_index: list(range(num_threads*worker_index,
                                                        num_threads*(worker_index+1)))
                               for worker_index in range(self._num_workers)}
+            allowed_cpus = sum([len(c) for c in available_cpus.values()])
 
-        # Eliminate cores corresponding to hyperthreading
-        for node_index, node_cpus in available_cpus.items():
-            node_cpus = [each for each in node_cpus if each < num_cpus]
-            available_cpus[node_index] = node_cpus
-
-        node_ids = list(available_cpus.keys())
-        num_nodes = len(available_cpus)
-        num_cpus_per_node = min([len(cpus) for cpus in available_cpus.values()])
-
-        # Distribute cores across workers
+        total_cpus = sum([len(c) for c in available_cpus.values()])
         worker_cpus = {}
         worker_nodes = {}
-        if num_nodes >= self._num_workers:
-            nodes_per_worker = num_nodes // self._num_workers
-            for worker_index in range(self._num_workers):
-                node_s = worker_index*nodes_per_worker
-                node_e = min((worker_index+1)*nodes_per_worker, num_nodes)
-                worker_cpus[worker_index] = sum([available_cpus[node_index]
-                                                 for node_index in node_ids[node_s:node_e]], [])
-                worker_nodes[worker_index] = node_ids[node_s:node_e]
-
-        else:
-            workers_per_node = self._num_workers // num_nodes
-            cpus_per_worker = num_cpus_per_node // workers_per_node
+        if total_cpus <= allowed_cpus:
+            # Eliminate cores corresponding to hyperthreading
             for node_index, node_cpus in available_cpus.items():
-                worker_s = node_index*workers_per_node
-                worker_e = min((node_index+1)*workers_per_node, self._num_workers)
-                worker_chunk = {}
-                for worker_index in range(worker_s, worker_e):
-                    cpu_s = worker_index*cpus_per_worker
-                    cpu_e = min((worker_index+1)*cpus_per_worker, len(node_cpus))
-                    worker_chunk[worker_index] = node_cpus[cpu_s:cpu_e]
-                    worker_nodes[worker_index] = [node_index]
-                worker_cpus.update(worker_chunk)
+                node_cpus = [each for each in node_cpus if each < num_cpus]
+                available_cpus[node_index] = node_cpus
+
+            node_ids = list(available_cpus.keys())
+            num_nodes = len(available_cpus)
+            num_cpus_per_node = min([len(cpus) for cpus in available_cpus.values()])
+
+            # Distribute cores across workers
+            worker_cpus = {}
+            worker_nodes = {}
+            if num_nodes >= self._num_workers:
+                nodes_per_worker = num_nodes // self._num_workers
+                for worker_index in range(self._num_workers):
+                    node_s = worker_index*nodes_per_worker
+                    node_e = min((worker_index+1)*nodes_per_worker, num_nodes)
+                    worker_cpus[worker_index] = sum([available_cpus[node_index]
+                                                     for node_index in node_ids[node_s:node_e]], [])
+                    worker_nodes[worker_index] = node_ids[node_s:node_e]
+
+            else:
+                workers_per_node = self._num_workers // num_nodes
+                cpus_per_worker = num_cpus_per_node // workers_per_node
+                for node_index, node_cpus in available_cpus.items():
+                    worker_s = node_index*workers_per_node
+                    worker_e = min((node_index+1)*workers_per_node, self._num_workers)
+                    worker_chunk = {}
+                    for worker_index in range(worker_s, worker_e):
+                        cpu_s = worker_index*cpus_per_worker
+                        cpu_e = min((worker_index+1)*cpus_per_worker, len(node_cpus))
+                        worker_chunk[worker_index] = node_cpus[cpu_s:cpu_e]
+                        worker_nodes[worker_index] = [node_index]
+                    worker_cpus.update(worker_chunk)
 
         # Initialise workers
         for worker_index in range(self._num_workers):
