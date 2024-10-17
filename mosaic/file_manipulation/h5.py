e224b4dbadfceca167753855b726b9e0adf0a1dc
Carlos Cueto <c.cueto@imperial.ac.uk>
2024-10-17 10:41:58 +0100

    Changed saving format

--- mosaic/file_manipulation/h5.py
+++ mosaic/file_manipulation/h5.py
@@ -92,8 +92,7 @@ def append(name, obj, group):
             append(sub_group_name, obj[index], sub_group)
 
     else:
-        if name not in group:
-            _write_dataset(name, obj, group)
+        _write_dataset(name, obj, group)
 
 
 def _write_dataset(name, obj, group):
@@ -128,31 +127,43 @@ def read(obj, lazy=True, filter=None, only=None):
         if filter is None:
             filter = {}
 
-        for key, filter_list in filter.items():
-            if key in obj:
-                value = _read_dataset(obj[key], lazy=False)
-                if value not in filter_list:
-                    raise FilterException
+        # for key, filter_list in filter.items():
+        #     if key in obj:
+        #         value = _read_dataset(obj[key], lazy=False)
+        #         if value not in filter_list:
+        #             raise FilterException
 
         if obj.attrs.get('is_array'):
             data = []
             for key in sorted(obj.keys()):
                 if only is not None and key not in only:
                     continue
-                try:
-                    value = read(obj[key], lazy=lazy, filter=filter)
-                except FilterException:
-                    continue
+                if filter is not None and key in filter:
+                    try:
+                        value = read(obj[key], lazy=lazy, filter=filter, only=filter[key])
+                    except FilterException:
+                        continue
+                else:
+                    try:
+                        value = read(obj[key], lazy=lazy, filter=filter)
+                    except FilterException:
+                        continue
                 data.append(value)
         else:
             data = {}
             for key in obj.keys():
                 if only is not None and key not in only:
                     continue
-                try:
-                    value = read(obj[key], lazy=lazy, filter=filter)
-                except FilterException:
-                    continue
+                if filter is not None and key in filter:
+                    try:
+                        value = read(obj[key], lazy=lazy, filter=filter, only=filter[key])
+                    except FilterException:
+                        continue
+                else:
+                    try:
+                        value = read(obj[key], lazy=lazy, filter=filter)
+                    except FilterException:
+                        continue
                 data[key] = value
 
         return data
@@ -306,6 +317,12 @@ class HDF5:
         self._file.close()
 
     def load(self, lazy=True, filter=None, only=None):
+        if filter is not None:
+            for k, v in filter.items():
+                if not isinstance(v, list):
+                    v = [v]
+                v = [str(v_) if not isinstance(v_, str) else v_ for v_ in v]
+                filter[k] = v
         group = self._file['/']
         description = read(group, lazy=lazy, filter=filter, only=only)
         return Struct(description)
